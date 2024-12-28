62cfb2e13dea7b545ab8b6e6cabad1578a69406c
iwindon <ivan.windon@icloud.com>
2024-12-28 10:04:58 -0500

    added circleshape

--- main.py
+++ main.py
@@ -6,11 +6,17 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     screen.fill((0, 0, 0))
     pygame.display.flip()
+    clock = pygame.time.Clock()
+    dt = 0
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+        dt = clock.tick(60) / 1000 # limit to 60 fps and convert from ms to seconds
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
