e8d47ae169dca579f31a43b38d01859993b519b5
iwindon <ivan.windon@icloud.com>
2024-12-29 16:46:12 -0500

    added player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init
@@ -9,10 +10,22 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     running = True
+
+    # Instantiate Player object
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+        # Clear screen
+        screen.fill((0, 0, 0))
+
+        # Draw player
+        player.draw(screen)
+
+        # Update display
+        pygame.display.flip()
 
         dt = clock.tick(60) / 1000 # limit to 60 fps and convert from ms to seconds
 
