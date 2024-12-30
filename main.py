fbdd95bd048e7db2d2b2f70cd2d2dcc44bcec55e
iwindon <ivan.windon@icloud.com>
2024-12-30 11:51:43 -0500

    added asteroids

--- main.py
+++ main.py
@@ -1,45 +1,47 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
-    pygame.init
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
 
-    # Instantiate Player object
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    # Create groups
-    updatable = [player]
-    drawable = [player]
+    dt = 0
 
-    # Game Loop 
-    running = True
-    while running:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                return
 
-        #player.update(dt)
         for obj in updatable:
             obj.update(dt)
 
-        # Clear screen
-        screen.fill((0, 0, 0))
+        screen.fill("black")
 
-        # Draw player
-        #player.draw(screen)
         for obj in drawable:
             obj.draw(screen)
 
-        # Update display
         pygame.display.flip()
 
-        dt = clock.tick(60) / 1000 # limit to 60 fps and convert from ms to seconds
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
-    pygame.quit()
 
 if __name__ == "__main__":
     main()
